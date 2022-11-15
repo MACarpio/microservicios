26673f5d424005acf434e26e4cd3aaba0412e738
Mauricio Apaza Carpio <mpacarpio@gmail.com>
2022-11-15 15:04:38 -0500

    dbmid context

--- context/SqlServer/DbVFmid.cs
+++ context/SqlServer/DbVFmid.cs
@@ -2,12 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using ms.Models.VFmid;
 namespace ms.Context.SqlServer
 {
-    public partial class DbVFmid : DbContext
+    public class DbVFmid : DbContext
     {
         public DbVFmid(DbContextOptions<DbVFmid> options) : base(options)
         {
         }
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
         public DbSet<md_turno_instalacion>? md_turno_instalacion { get; set; }
         public DbSet<md_sv>? md_sv { get; set; }
         public DbSet<md_busquedas>? md_busquedas { get; set; }

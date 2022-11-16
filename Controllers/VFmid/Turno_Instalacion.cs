204e67d83f731020e17b268a522f64f339efef4e
Mauricio Apaza Carpio <mpacarpio@gmail.com>
2022-11-16 16:14:25 -0500

    Turno instalacion zona horaria

--- Controllers/VFmid/Turno_Instalacion.cs
+++ Controllers/VFmid/Turno_Instalacion.cs
@@ -60,8 +60,11 @@ namespace ms.Controllers.VFmid
         [HttpGet("GetTurnoInstalacion")]
         public ActionResult<string> GetTurnoInstalacion(string fecha, string distrito, int flag)
         {
-            DateTime hoy = DateTime.Now;
+            DateTime horaPeticion = DateTime.Now;
+            TimeZoneInfo Peru = TimeZoneInfo.FindSystemTimeZoneById("SA Pacific Standard Time");
+            var hoy = TimeZoneInfo.ConvertTime(horaPeticion, Peru, TimeZoneInfo.Local);
             var tr_instalacion = _dbmid.md_turno_instalacion.FirstAsync(x => x.Distrito == distrito);
+            DateTime dateTime = DateTime.Parse(fecha);
             if (tr_instalacion.Result.Tr_noche == "")
             {
                 tr_instalacion.Result.Tr_noche = "No Disponible";
@@ -70,56 +73,63 @@ namespace ms.Controllers.VFmid
             {
                 return NotFound("No se encontró el distrito");
             }
-            if (flag == 0)
+            if (DateOnly.FromDateTime(dateTime) < DateOnly.FromDateTime(hoy))
             {
-                if (fecha == DateOnly.FromDateTime(hoy).ToString())
-                {
-                    if (hoy.Hour >= 0 && hoy.Hour < 6)
-                    {
-                        return Ok(new { tr_mañana = tr_instalacion.Result.Tr_tarde, tr_tarde = tr_instalacion.Result.Tr_tarde, tr_noche = tr_instalacion.Result.Tr_noche });
-                    }
-                    else
-                    {
-                        if (hoy.Hour >= 6 && hoy.Hour < 11)
-                        {
-                            return Ok(new { tr_mañana = "No Disponible", tr_tarde = tr_instalacion.Result.Tr_tarde, tr_noche = tr_instalacion.Result.Tr_noche });
-                        }
-                        else
-                        {
-                            if (hoy.Hour >= 11 && (hoy.Hour <= 14 && hoy.Minute <= 30))
-                            {
-                                return Ok(new { tr_mañana = "No Disponible", tr_tarde = "No Disponible", tr_noche = tr_instalacion.Result.Tr_noche });
-                            }
-                            else
-                            {
-                                return Ok(new { tr_mañana = "No Disponible", tr_tarde = "No Disponible", tr_noche = "No Disponible" });
-                            }
-                        }
-                    }
-
-                }
-                else
-                {
-                    return Ok(new { tr_mañana = tr_instalacion.Result.Tr_mañana, tr_tarde = tr_instalacion.Result.Tr_tarde, tr_noche = tr_instalacion.Result.Tr_noche });
-                }
-            }
-            if (flag == 1)
-            {
-                DateTime dateTime = DateTime.Parse(fecha);
-                if (dateTime <= hoy.AddDays(2))
-                {
-                    return Ok(new { message = "No se puede agendar antes de los 2 dias" });
-                }
-                else
-                {
-                    return Ok(new { tr_mañana = tr_instalacion.Result.Tr_mañana, tr_tarde = tr_instalacion.Result.Tr_tarde, tr_noche = tr_instalacion.Result.Tr_noche });
-                }
-
+                return NotFound("No se puede agendar en fechas pasadas");
             }
             else
             {
-                return NotFound("Flag Solo Acepta 0 o 1");
+                if (flag == 0)
+                {
+                    if (fecha == DateOnly.FromDateTime(hoy).ToString())
+                    {
+                        if (hoy.Hour >= 0 && hoy.Hour < 6)
+                        {
+                            return Ok(new { tr_mañana = tr_instalacion.Result.Tr_tarde, tr_tarde = tr_instalacion.Result.Tr_tarde, tr_noche = tr_instalacion.Result.Tr_noche });
+                        }
+                        else
+                        {
+                            if (hoy.Hour >= 6 && hoy.Hour < 11)
+                            {
+                                return Ok(new { tr_mañana = "No Disponible", tr_tarde = tr_instalacion.Result.Tr_tarde, tr_noche = tr_instalacion.Result.Tr_noche });
+                            }
+                            else
+                            {
+                                if (hoy.Hour >= 11 && (hoy.Hour <= 14 && hoy.Minute <= 30))
+                                {
+                                    return Ok(new { tr_mañana = "No Disponible", tr_tarde = "No Disponible", tr_noche = tr_instalacion.Result.Tr_noche });
+                                }
+                                else
+                                {
+                                    return Ok(new { tr_mañana = "No Disponible", tr_tarde = "No Disponible", tr_noche = "No Disponible" });
+                                }
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        return Ok(new { tr_mañana = tr_instalacion.Result.Tr_mañana, tr_tarde = tr_instalacion.Result.Tr_tarde, tr_noche = tr_instalacion.Result.Tr_noche });
+                    }
+                }
+                if (flag == 1)
+                {
+                    if (DateOnly.FromDateTime(dateTime) <= DateOnly.FromDateTime(hoy.AddDays(2)))
+                    {
+                        return Ok(new { message = "No se puede agendar antes de los 2 dias" });
+                    }
+                    else
+                    {
+                        return Ok(new { tr_mañana = tr_instalacion.Result.Tr_mañana, tr_tarde = tr_instalacion.Result.Tr_tarde, tr_noche = tr_instalacion.Result.Tr_noche });
+                    }
+
+                }
+                else
+                {
+                    return NotFound("Flag Solo Acepta 0 o 1");
+                }
             }
+
         }
     }
 }

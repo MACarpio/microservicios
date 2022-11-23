433fc8d95b2f7c0938a26e3ff78412caccb12b45
Mauricio Apaza Carpio <mpacarpio@gmail.com>
2022-11-23 17:36:01 -0500

    new Lead

--- Controllers/VFmid/Lead.cs
+++ Controllers/VFmid/Lead.cs
@@ -7,43 +7,13 @@ namespace ms.Controllers.VFmid
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class LeadSinCobertura : ControllerBase
+    public class Lead : ControllerBase
     {
         private readonly DbVFmid _contextVFmid;
 
-        public LeadSinCobertura(DbVFmid contextVFmid)
+        public Lead(DbVFmid contextVFmid)
         {
             _contextVFmid = contextVFmid;
-        }
-
-        [HttpPost("Leads")]
-        public ActionResult<IEnumerable<tp_clientes>> RegLead(string motivo, ModelLead model)
-        {
-            var cli = GetCliente(model.cliente.cli_num_doc);
-            if (cli.Result == null)
-            {
-                tp_clientes cliente = new tp_clientes
-                {
-                    ide_tip_viv = model.cliente.ide_tip_viv,
-                    ide_tip_doc = model.cliente.ide_tip_doc,
-                    cli_nom = model.cliente.cli_nom,
-                    cli_ape_pat = model.cliente.cli_ape_pat,
-                    cli_ape_mat = model.cliente.cli_ape_mat,
-                    cli_num_doc = model.cliente.cli_num_doc,
-                    cli_tel1 = model.cliente.cli_tel1,
-                    cli_email = model.cliente.cli_email
-                };
-                RegistrarCliente(cliente);
-                var cli2 = GetCliente(cliente.cli_num_doc);
-                RegistrarLead(motivo, model, cli2.Result);
-                return Ok(new { message = "Cliente registrado y Lead registrado", Cliente = cli2.Result });
-            }
-            else
-            {
-                RegistrarLead(motivo, model, cli.Result);
-                return Ok(new { message = "Lead registrado", Cliente = cli.Result });
-            }
-
         }
 
         [NonAction]
@@ -54,26 +24,26 @@ namespace ms.Controllers.VFmid
         }
 
         [NonAction]
-        public void RegistrarLead(string motivo, ModelLead lead, tp_clientes cliente)
+        public void RegistrarLead(ModelLeadCompleto lead, tp_clientes cliente)
         {
             var busqueda = new tp_i_busquedas
             {
                 ide_i_cli = Convert.ToInt32(cliente.ide_cli),
-                ide_i_soc = lead.ide_soc,
-                ide_i_pre_soc = lead.ide_pre_soc,
-                i_ide_edi = lead.ide_edi,
-                i_ide_tip_int = lead.ide_tip_int,
-                i_bus_tip = lead.bus_tip,
-                i_bus_dir_ref = lead.bus_dir_ref,
+                ide_i_soc = lead.ide_i_soc,
+                ide_i_pre_soc = lead.ide_i_pre_soc,
+                i_ide_edi = lead.i_ide_edi,
+                i_ide_tip_int = lead.i_ide_tip_int,
+                i_bus_tip = lead.i_bus_tip,
+                i_bus_dir_ref = lead.i_bus_dir_ref,
                 i_bus_ubigeo = lead.geofinder.bus_ubigeo,
                 i_bus_res = lead.geofinder.bus_res,
                 i_bus_lat = lead.geofinder.bus_lat,
                 i_bus_lng = lead.geofinder.bus_lng,
                 i_bus_dir = lead.geofinder.bus_dir,
-                i_bus_obs = motivo,
-                i_num_departament = lead.num_departament,
-                i_bus_seg_est = lead.bus_seg_est,
-                i_seg_prox_fecha = lead.seg_prox_fecha,
+                i_bus_obs = lead.i_bus_obs,
+                i_num_departament = lead.i_num_departament,
+                i_bus_seg_est = lead.i_bus_seg_est,
+                i_seg_prox_fecha = lead.i_seg_prox_fecha,
                 i_seller_id = 15,
                 i_seller_type = "Autoventa",
                 cli_ubi_dep = lead.cli_ubi_dep,
@@ -102,6 +72,37 @@ namespace ms.Controllers.VFmid
         {
             _contextVFmid.tp_clientes.Add(cliente);
             _contextVFmid.SaveChanges();
+        }
+
+
+        [HttpPost("Nuevo")]
+        public ActionResult<IEnumerable<tp_clientes>> NuevoLead(ModelLeadCompleto model)
+        {
+            var cli = GetCliente(model.cliente.cli_num_doc);
+            if (cli.Result == null)
+            {
+                tp_clientes cliente = new tp_clientes
+                {
+                    ide_tip_viv = model.cliente.ide_tip_viv,
+                    ide_tip_doc = model.cliente.ide_tip_doc,
+                    cli_nom = model.cliente.cli_nom,
+                    cli_ape_pat = model.cliente.cli_ape_pat,
+                    cli_ape_mat = model.cliente.cli_ape_mat,
+                    cli_num_doc = model.cliente.cli_num_doc,
+                    cli_tel1 = model.cliente.cli_tel1,
+                    cli_email = model.cliente.cli_email
+                };
+                RegistrarCliente(cliente);
+                var cli2 = GetCliente(cliente.cli_num_doc);
+                RegistrarLead(model, cli2.Result);
+                return Ok(new { message = "Cliente registrado y Lead registrado", Cliente = cli2.Result });
+            }
+            else
+            {
+                RegistrarLead(model, cli.Result);
+                return Ok(new { message = "Lead registrado", Cliente = cli.Result });
+            }
+
         }
     }
 }
--- Models/VFmid/tp_i_busquedas.cs
+++ Models/VFmid/tp_i_busquedas.cs
@@ -8,20 +8,28 @@ namespace ms.Models.VFmid
     {
         [Key]
         public long ide_i_bus { get; set; }
+        [Required]
         public long ide_i_cli { get; set; }
         public long? ide_i_soc { get; set; }
         public long? ide_i_pre_soc { get; set; }
         public long? i_ide_edi { get; set; }
+        [Required]
         public long i_ide_tip_int { get; set; }
+        [Required]
         public int i_bus_tip { get; set; }
         public string? i_bus_dir_ref { get; set; }
+        [Required]
         public string i_bus_ubigeo { get; set; } = null!;
         public string? i_bus_res { get; set; }
+        [Required]
         public string i_bus_lat { get; set; } = null!;
+        [Required]
         public string i_bus_lng { get; set; } = null!;
+        [Required]
         public string i_bus_dir { get; set; } = null!;
         public string? i_bus_obs { get; set; }
         public string? i_num_departament { get; set; }
+        [Required]
         public int i_bus_seg_est { get; set; }
         public DateTime? i_seg_prox_fecha { get; set; }
         public long? i_seller_id { get; set; }
@@ -30,9 +38,11 @@ namespace ms.Models.VFmid
         public string? cli_ubi_dis { get; set; }
         public string? cli_ubi_prov { get; set; }
         public string? cond_torre { get; set; }
+        [StringLength(5)]
         public string? score_equifax { get; set; }
         public string? bus_cod_postal { get; set; }
         public string? bus_cod_postal_text { get; set; }
+        [StringLength(4)]
         public string? num_piso { get; set; }
         public int? ide_pre_rel { get; set; }
         public long? ide_venta_origen { get; set; }
@@ -49,28 +59,44 @@ namespace ms.Models.VFmid
     }
 
     [NotMapped]
-    public class ModelLead
+    public class ModelPrGeofinder
     {
-        public ModelPrGeofinder? geofinder { get; set; }
+        public string bus_ubigeo { get; set; } = null!;
+        public string? bus_res { get; set; }
+        public string bus_lat { get; set; } = null!;
+        public string bus_lng { get; set; } = null!;
+        public string bus_dir { get; set; } = null!;
+    }
+
+    [NotMapped]
+    public class ModelLeadCompleto
+    {
+        [Required]
         public modelCliente? cliente { get; set; }
-        public long? ide_soc { get; set; }
-        public long? ide_pre_soc { get; set; }
-        public long? ide_edi { get; set; }
-        public long ide_tip_int { get; set; }
-        public int bus_tip { get; set; }
-        public string? bus_dir_ref { get; set; }
-        public string? bloque { get; set; }
-        public int? tipo_interes_Id { get; set; }
-        public string? num_departament { get; set; }
-        public int bus_seg_est { get; set; }
-        public DateTime? seg_prox_fecha { get; set; }
+        public long? ide_i_soc { get; set; }
+        public long? ide_i_pre_soc { get; set; }
+        public long? i_ide_edi { get; set; }
+        [Required]
+        public long i_ide_tip_int { get; set; }
+        [Required]
+        public int i_bus_tip { get; set; }
+        public string? i_bus_dir_ref { get; set; }
+        [Required]
+        public ModelPrGeofinder? geofinder { get; set; }
+        public string? i_bus_obs { get; set; }
+        public string? i_num_departament { get; set; }
+        [Required]
+        public int i_bus_seg_est { get; set; }
+        public DateTime? i_seg_prox_fecha { get; set; }
         public string? cli_ubi_dep { get; set; }
         public string? cli_ubi_dis { get; set; }
         public string? cli_ubi_prov { get; set; }
         public string? cond_torre { get; set; }
+        [StringLength(5)]
         public string? score_equifax { get; set; }
         public string? bus_cod_postal { get; set; }
         public string? bus_cod_postal_text { get; set; }
+        [StringLength(4)]
         public string? num_piso { get; set; }
         public int? ide_pre_rel { get; set; }
         public long? ide_venta_origen { get; set; }
@@ -80,14 +106,5 @@ namespace ms.Models.VFmid
         public long? paso_proceso { get; set; }
         public long? id_consulta_web { get; set; }
         public long? estado { get; set; }
-    }
-    [NotMapped]
-    public class ModelPrGeofinder
-    {
-        public string bus_ubigeo { get; set; } = null!;
-        public string? bus_res { get; set; }
-        public string bus_lat { get; set; } = null!;
-        public string bus_lng { get; set; } = null!;
-        public string bus_dir { get; set; } = null!;
     }
 }
